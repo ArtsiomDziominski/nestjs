23357bea7ca33ad75db3041201227b80f6b29ac1
”Artsiom <artsiomdziominski@gmail.com>
2022-12-04 01:18:56 +0400

    add const

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,13 @@ import { NewOrderController } from './new-order/new-order.controller';
 import { CurrentOrderController } from './current-order/current-order.controller';
 import {HttpRequestService} from "./service/http-request.service";
 import { BalanceController } from './balance/balance.controller';
+import { MarketOrderController } from './market-order/market-order.controller';
+import { CancelOpenOrdersController } from './cancel-open-orders/cancel-open-orders.controller';
+import { IncomeHistoryController } from './income-history/income-history.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, NewOrderController, CurrentOrderController, BalanceController],
+  controllers: [AppController, NewOrderController, CurrentOrderController, BalanceController, MarketOrderController, CancelOpenOrdersController, IncomeHistoryController],
   providers: [AppService, HttpRequestService],
 })
 export class AppModule {}
